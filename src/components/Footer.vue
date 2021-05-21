b0b4e8cda43b461c0eb74fd2658aedb8cf70c2c4
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-05-21 15:37:54 +0200

    Created Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+      <!-- Footer -->
+<footer class="fixed-bottom page-footer font-small bg-danger">
+
+ <!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2021 Copyright:
+<a href="https://www.lexicon.se/"> Lexicon.com</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
