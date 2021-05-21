28906c179aa0b0d22ee73ea92726b637f1b63c4a
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-05-21 15:38:47 +0200

    Added components in the app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
+    <Hero></Hero>
+    <AppFooter></AppFooter>
   </div>
 </template>
 
 <script>
-
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 export default {
   name: 'App',
   components: {
-    
+    Navbar,
+    AppFooter: Footer,
+    Hero,
   }
 }
 </script>
