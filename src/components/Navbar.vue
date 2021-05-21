ef62aa457415375071ef32aac9c90ae6cdd13ea4
hens.virtuella.varld <hens.virtuella.varld@gmail.com>
2021-05-21 15:37:38 +0200

    Created Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.lexicon.se/Static/images/logo-lexicon_2x.gif" alt="" width="120" height="40" class="d-inline-block align-text-top">
+    </a>
+  </div>
+</nav>
+    </div>
+
+</template>
+
+
+
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+
+
+
+<style>
+
+</style>
